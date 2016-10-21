55d24efdfbe7b0d60e38d9e8cb29657965c548f2
hannrl <hannrl6@gmail.com>
2016-10-20 23:21:13 -0400

    first submission - ready

--- js/map.js
+++ js/map.js
@@ -82,144 +82,151 @@ var restaurantList = [{
 }];
 
 var viewModel = function() {
-        var self = this;
+    var self = this;
 
-        self.genreList = ko.observableArray();
-        self.markers = ko.observableArray();
-        self.currentFilter = ko.observable(""); // Initialize with an empty string
+    self.genreList = ko.observableArray();
+    self.markers = ko.observableArray();
+    self.currentFilter = ko.observable(""); // Initialize with an empty string
 
-        var infowindow = new google.maps.InfoWindow();
-        var marker;
-        var infowindows = [];
-        self.zmtAddress;
-        self.zmtCost;
-        self.zmtThumb;
-        self.infoWindowContent;
+    var infowindow = new google.maps.InfoWindow();
+    var marker;
+    var infowindows = [];
+    self.zmtAddress;
+    self.zmtThumb;
+    self.infoWindowContent;
+    self.zmtName;
 
-        // marker creation
-        for (i = 0; i < restaurantList.length; i++) {
+    // marker creation
+    for (i = 0; i < restaurantList.length; i++) {
 
-            var marker = new google.maps.Marker({
-                position: restaurantList[i].position,
-                title: restaurantList[i].title,
-                show: ko.observable(restaurantList[i].show),
-                genre: restaurantList[i].genre,
-                animation: google.maps.Animation.DROP,
-                map: map,
-                id: restaurantList[i].id,
-            });
-
-            if (restaurantList[i].show) {
-                self.markers.push(marker);
-            }
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    getZomatoInfo(marker.id); //makes ajax call to Zomato API using the restaurant's Zomato ID
-                    self.goToRestaurant(marker);
-                    marker.setAnimation(google.maps.Animation.BOUNCE);
-                    setTimeout(function(){ marker.setAnimation(null); }, 1500); //stops after two bounces
-                    infowindows.push(infowindow);
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
-
-        };
-
-        restaurantList.forEach(function(restaurant) {
-            var genre = restaurant.genre;
-
-            // populates unique genres only
-            if (!self.genreList().includes(genre)) {
-                self.genreList.push(genre);
-            }
+        var marker = new google.maps.Marker({
+            position: restaurantList[i].position,
+            title: restaurantList[i].title,
+            show: ko.observable(restaurantList[i].show),
+            genre: restaurantList[i].genre,
+            icon: 'images/nature-dog-bone-icons-4.png',
+            animation: google.maps.Animation.DROP,
+            map: map,
+            id: restaurantList[i].id
         });
 
+        if (restaurantList[i].show) {
+            self.markers.push(marker);
+        }
 
-
-
-        // filters and unfilters based on selection in drop-down
-        self.filteredMarkers = ko.computed(function() {
-            map.panTo({
-                lat: 35.211698,
-                lng: -80.857594
-            });
-            map.setZoom(15);
-            var filter = self.currentFilter();
-            if (!filter || self.currentFilter() === undefined) {
-                for (i = 0; i < self.markers().length; i++)
-                    self.markers()[i].setVisible(true); //shows all markers again when 'All' is chosen from drop-down
-                return self.markers();
-            } else {
-                return self.markers().filter(function(marker) {
-                    var genre = marker.genre;
-                    var match = genre.indexOf(filter) !== -1;
-                    self.hideUnselected(marker, match);
-                    return match;
-                });
-
-
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                getZomatoInfo(marker.id); //makes ajax call to Zomato API using the restaurant's Zomato ID
+                self.goToRestaurant(marker);
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                    marker.setAnimation(null);
+                }, 1500); //stops after two bounces
+                infowindows.push(infowindow);
+                infowindow.open(map, marker);
             }
+        })(marker, i));
+
+    };
+
+    restaurantList.forEach(function(restaurant) {
+        var genre = restaurant.genre;
+
+        // populates unique genres only
+        if (!self.genreList().includes(genre)) {
+            self.genreList.push(genre);
+        }
+    });
+
+
+
+
+    // filters and unfilters based on selection in drop-down
+    self.filteredMarkers = ko.computed(function() {
+        map.panTo({
+            lat: 35.211698,
+            lng: -80.857594
         });
-        //hides the markers of locations that didn't match the genre selected or shows the markers that did match
-        self.hideUnselected = function(marker, match) {
-            if (match === false) {
-                marker.setVisible(false);
-            } else {
-                marker.setVisible(true);
+        map.setZoom(15);
+        var filter = self.currentFilter();
+        if (!filter || self.currentFilter() === undefined) {
+            for (i = 0; i < self.markers().length; i++)
+                self.markers()[i].setVisible(true); //shows all markers again when 'All' is chosen from drop-down
+            return self.markers();
+        } else {
+            return self.markers().filter(function(marker) {
+                var genre = marker.genre;
+                var match = genre.indexOf(filter) !== -1;
+                self.hideUnselected(marker, match);
+                return match;
+            });
+
+
+        }
+    });
+    //hides the markers of locations that didn't match the genre selected or shows the markers that did match
+    self.hideUnselected = function(marker, match) {
+        if (match === false) {
+            marker.setVisible(false);
+        } else {
+            marker.setVisible(true);
+        }
+        self.closeInfoWindows();
+    }
+
+    //closes all infowindows when called
+    self.closeInfoWindows = function() {
+        for (var i = 0; i < infowindows.length; i++) {
+            infowindows[i].close();
+        }
+    }
+
+
+
+    //resets map to original center
+    self.centerMap = function() {
+        map.panTo(new GLatLng(35.211698, -80.857594));
+    }
+
+
+    //zooms to and opens infowindow of restaurant selected from the right nav
+    self.goToRestaurant = function(clickedRestaurant) {
+        clickedRestaurant.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            clickedRestaurant.setAnimation(null);
+        }, 1500); //stops after two bounces
+        for (i = 0; i < self.markers().length; i++) {
+            if (clickedRestaurant.title === self.markers()[i].title) {
+                getZomatoInfo(clickedRestaurant.id);
+                map.panTo(self.markers()[i].position);
+                map.setZoom(16);
+                clickedRestaurant.setVisible(true);
+                infowindow.open(map, self.markers()[i]);
             }
-            self.closeInfoWindows();
         }
+    };
+    // calls Zomato async and put returned info into infowindow
+    function getZomatoInfo(restaurantId) {
+        var zomatoUrl = "https://developers.zomato.com/api/v2.1/restaurant?apikey=9f57bb53795b372e56c81c59706568ca&res_id=";
+        var restId = restaurantId;
 
-        //closes all infowindows when called
-        self.closeInfoWindows = function() {
-            for (var i = 0; i < infowindows.length; i++) {
-                infowindows[i].close();
+        $.ajax({
+            url: zomatoUrl + restId,
+            dataType: 'jsonp',
+            success: function(data) {
+                self.zmtAddress = data.location.address;
+                self.zmtCuisines = data.cuisines;
+                self.zmtThumb = data.thumb;
+                self.zmtName = data.name;
+                self.infoWindowContent = '<div id = "restaurantThumb"><img src="' + self.zmtThumb + '"></div><div id= "infowindow">' + self.zmtName + '<br>' + self.zmtAddress + '<br>' + self.zmtCuisines + '</div>'
+                infowindow.setContent(self.infoWindowContent);
+                console.log(self.zmtCost);
+            },
+            error: function() {
+                alert("We couldn't reach Zomato right now. Please try again soon.")
             }
-        }
+        })
 
-
-
-        //resets map to original center
-        self.centerMap = function() {
-            map.panTo(new GLatLng(35.211698, -80.857594));
-        }
-
-
-        //zooms to and opens infowindow of restaurant selected from the right nav
-        self.goToRestaurant = function(clickedRestaurant) {
-            clickedRestaurant.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function(){ clickedRestaurant.setAnimation(null); }, 1500); //stops after two bounces
-            for (i = 0; i < self.markers().length; i++) {
-                if (clickedRestaurant.title === self.markers()[i].title) {
-                    getZomatoInfo(clickedRestaurant.id);
-                    map.panTo(self.markers()[i].position);
-                    map.setZoom(18);
-                    clickedRestaurant.setVisible(true);
-                    infowindow.open(map, self.markers()[i]);
-                }
-            }
-        };
-
-        function getZomatoInfo(restaurantId) {
-            var zomatoUrl = "https://developers.zomato.com/api/v2.1/restaurant?apikey=9f57bb53795b372e56c81c59706568ca&res_id=";
-            var restId = restaurantId;
-
-            $.ajax({
-                url: zomatoUrl + restId,
-                dataType: 'jsonp',
-                success: function(data) {
-                    self.zmtAddress = data.location.address;
-                    self.zmtCost = data.average_cost_for_two;
-                    self.zmtThumb = data.thumb;
-                    self.zmtName = data.name;
-                    console.log(self.zmtName);
-                    self.infoWindowContent = '<div id= "infowindow">' + self.zmtAddress + '<br>' + self.zmtName + '</div>';
-                    infowindow.setContent(self.infoWindowContent);
-                }
-            })
-
-
-        }
+    }
 }
-        ko.applyBindings(new viewModel());
+ko.applyBindings(new viewModel());
